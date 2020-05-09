1d06d9146ae4fd67062ed2c13e4511fabd0e5013
Bsacco9814 <Brandon.Sacco13@gmail.com>
2020-05-09 16:46:11 -0500

    added1stQuestions

--- js/main.js
+++ js/main.js
@@ -24,6 +24,8 @@ startButton.addEventListener('click', startGame)
 function startGame() {
     console.log('Started');
     startButton.classList.add('hide')
+    questionContentElement.classList.remove('hide')
+    setNextQuestion()
 }
 
 function setNextQuestion() {
@@ -33,3 +35,19 @@ function setNextQuestion() {
 function selectAnswer() {
 
 } 
+
+
+
+/*----- Question references -----*/
+const question = [
+    {
+      question: 'The only known monotremes in the animal kingdom are the echidna and which other creature?'
+      answers: [
+          {text: 'Platypus', correct: true},
+          {text: 'Saola', correct: false},
+          {text: 'Hirola', correct: false},
+          {text: 'Baiji', correct: false},
+
+      ]  
+    }
+]
